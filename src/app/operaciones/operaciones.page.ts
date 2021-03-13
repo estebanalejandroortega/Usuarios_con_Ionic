db6139bdbc715ac5792e4a3f818d6b8134486c48
Esteban <estebanalejandroortega@hotmail.com>
2021-03-13 11:31:06 -0500

    Operar

--- src/app/operaciones/operaciones.page.ts
+++ src/app/operaciones/operaciones.page.ts
@@ -5,16 +5,44 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './operaciones.page.html',
   styleUrls: ['./operaciones.page.scss'],
 })
-export class OperacionesPage implements OnInit {
+export class OperacionesPage {
 
-  saludar()
-  {
+  num1 = null;
+  num2 = null;
+  operacion = null;
+  rta = null;
+  strOp = null;
+  
+  
+  calcular(){
+    let numero1 = parseInt(this.num1)
+    let numero2 = parseInt(this.num2)
     
-    
-  }
+    switch(this.operacion)
+    {
+      case '+':
+        this.strOp = 'suma'
+        this.rta = numero1 + numero2
+      break;
+
+      case '-':
+        this.strOp = 'resta'
+        this.rta = numero1 - numero2
+      break;
+
+      case '*':
+        this.strOp = 'multiplicación'
+        this.rta = numero1 * numero2
+      break;
+
+      case '/':
+        this.strOp = 'división'
+        this.rta = numero1 / numero2
+      break;
+    }
+  } 
   constructor() { }
 
   ngOnInit() {
   }
-
 }
